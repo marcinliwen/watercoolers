34ea736e490dae6b69d422d9434934b6da391eeb
liwenmarcin <marcin.liwen@sohoshop.pl>
2022-04-14 00:47:02 +0200

    netlify CMS

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -47,6 +47,7 @@ const TemplateWrapper = ({ children }) => {
           property="og:image"
           content={`${withPrefix("/")}img/og-image.jpg`}
         />
+        <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Helmet>
       <Navbar />
       <main>{children}</main>
