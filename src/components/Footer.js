266553677f731f8b8467c82805dbde671191a04a
marcinliwen <liwen.marcin@gmail.com>
2022-04-21 15:21:50 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,11 +13,20 @@ const Footer = class extends React.Component {
       <footer className="bg-blue py-10">
 
         <div className="container py-2">
+          <div className="flex flex-wrap gap-16">
           <img
             src={logo}
             alt="We"
-            style={{ width: "126px", height: "auto" }}
+            style={{ width: "300px", height: "auto" }}
           />
+          <div className="text-white text-sm font-light flex flex-col gap-4">
+            <a href="/">CONTACT</a>
+            <a href="/">TERMS & CONDITIONS FOR EXHIBITORS</a>
+            <a href="/">GENERAL INFORMATION</a>
+            <a href="/">PRIVATE POLICY</a>
+          </div>
+          </div>
+          
         </div>
       </footer>
     );
