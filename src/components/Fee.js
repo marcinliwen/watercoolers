d09b3659804bd2b9cecdb1d0fa215423965b1798
liwenmarcin <marcin.liwen@sohoshop.pl>
2022-05-13 23:10:01 +0200

    fix fee 2

--- src/components/Fee.js
+++ src/components/Fee.js
@@ -6,173 +6,233 @@ const Fee = () => {
       <div className="container">
         <div className="fee-wrapper">
           <div className="fee-col-titles">
-          <div className="fee-header"></div>
-          
-          <div className="fee-row">
-                <span className="col-span-1 text-left font-bold">
-                FAIR ENTRACE{" "}
-                </span>
-                </div>
-              <div className="fee-row"><span className="col-span-1 text-left font-bold">CONFERENCE</span></div>
-              <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">GALA DINNER </span></div>
+            <div className="fee-header"></div>
+
             <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CITY TOUR</span></div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">PACKAGE 1:</span></div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold ">PACKAGE 2:</span></div>
-            </div>
-          <div className="fee-col">
-              <div className="fee-header">
-            <span className="font-bold">SPECIAL OFFER <br />
-            FOR EXHIBITORS </span>
-            <span className=" text-sm text-center">
-              EARLY BIRD RATES,<br />
-              IF BOOKED BEFORE  <br /><strong>30 SEPTEMBER</strong>{" "}
-            </span>
-            <span className=" text-sm text-center">
-              REGULAR RATES, <br /> FROM <br /><strong>1 OCTOBER</strong>{" "}
-            </span>
-            </div>
-            <div className="fee-row">
-                <span className="col-span-1 text-left font-bold">
-                FAIR ENTRACE{" "}
-                </span>
-                <span className="col-span-2  ">FREE </span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CONFERENCE</span>
-            <span className=" ">169 €</span>
-            <span className=" ">205 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">GALA DINNER </span>
-            <span className=" ">149 €</span>
-            <span className=" ">175 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CITY TOUR</span>
-            <span className=" ">79 €</span>
-            <span className=" ">87 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">PACKAGE 1:</span>
-            <span className=" ">409 €</span>
-            <span className=" ">467 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold ">PACKAGE 2:</span>
-            <span className=" ">279 €</span>
-            <span className=" ">321 €</span>
-            </div>
-          </div>
-          <div className="fee-col">
-              <div className="fee-header">
-          <span className="col-span-2  font-bold">
-              WE MEMBERS & NATIONAL <br /> ASSOCIATIONS
-            </span>
-            <span className=" text-sm text-center">
-              EARLY BIRD RATES, <br />
-              IF BOOKED BEFORE <br /><strong>30 SEPTEMBER</strong>{" "}
-            </span>
-            <span className=" text-sm text-center">
-              REGULAR RATES, <br /> FROM <br /><strong>1 OCTOBER</strong>{" "}
-            </span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">
-              FAIR ENTRACE{" "}
-            </span>
-            <span className="col-span-2  ">FREE </span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CONFERENCE</span>
-            <span className=" ">198 €</span>
-            <span className=" ">228 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">GALA DINNER </span>
-            <span className=" ">149 €</span>
-            <span className=" ">175 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CITY TOUR</span>
-            <span className=" ">79 €</span>
-            <span className=" ">87 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">PACKAGE 1:</span>
-            <span className=" ">489 €</span>
-            <span className=" ">567 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold ">PACKAGE 2:</span>
-            <span className=" ">339 €</span>
-            <span className=" ">392 €</span>
-            </div>
-          </div>
-          <div className="fee-col">
-         
-            <div className="fee-header">
-                <span className="font-bold">NON-WE <br /
-                > MEMBERS</span>
-                <span className=" text-sm text-center">
-                    EARLY BIRD RATES, <br />
-                    IF BOOKED BEFORE <br /><strong>30 SEPTEMBER</strong>{" "}
-                </span>
-                <span className=" text-sm text-center">
-                    REGULAR RATES, <br /> FROM <br /><strong>1 OCTOBER</strong>{" "}
-                </span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">
-              FAIR ENTRACE{" "}
-            </span>
-            <span className="col-span-2  ">FREE </span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CONFERENCE</span>
-            <span className=" ">223 €</span>
-            <span className=" ">257 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">GALA DINNER </span>
-            <span className=" ">198 €</span>
-            <span className=" ">228 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">CITY TOUR</span>
-            <span className=" ">91 €</span>
-            <span className=" ">105 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold">PACKAGE 1:</span>
-            <span className=" ">531 €</span>
-            <span className=" ">613 €</span>
-            </div>
-            <div className="fee-row">
-            <span className="col-span-1 text-left font-bold ">PACKAGE 2:</span>
-            <span className=" ">369 €</span>
-            <span className=" ">421 €</span>
-            </div>
-          </div>
-        </div>
-        <div>
-        <div className="text-left font-bold">
-              PACKAGE 1:{" "}
-              <span className="text-pink">
-                CONFERENCE, WORKSHOPS, GALA DINNER
+              <span className="col-span-1 text-left font-bold">
+                FAIR ENTRANCE{" "}
               </span>
             </div>
-            <div className="text-left font-bold ">
-              PACKAGE 2:{" "}
-              <span className="text-pink">CONFERENCE, WORKSHOPS</span>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CITY TOUR</span>
             </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CONFERENCE</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                GALA DINNER{" "}
+              </span>
+            </div>
+
+            <div className="fee-row h-[84px]">
+              <span className="col-span-1 text-left font-bold">
+                PACKAGE 1:{" "}
+                <span className="text-pink text-sm text-[12px] font-normal">
+                  CONFERENCE, WORKSHOPS
+                </span>
+              </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold ">
+                PACKAGE 2:{" "}
+                <span className="text-pink text-[12px] font-normal">
+                  {" "}
+                  CONFERENCE, WORKSHOPS, <br />
+                  GALA DINNER
+                </span>
+              </span>
+            </div>
+          </div>
+          <div className="fee-col">
+            <div className="fee-header">
+              <span className="font-bold">
+                SPECIAL OFFER <br />
+                FOR EXHIBITORS{" "}
+              </span>
+              <span className=" text-sm text-center">
+                EARLY BIRD RATES
+                <br />
+                IF BOOKED BEFORE <br />
+                <strong>30 SEPTEMBER</strong>{" "}
+              </span>
+              <span className=" text-sm text-center">
+                REGULAR RATES <br /> FROM <br />
+                <strong>1 OCTOBER</strong>{" "}
+              </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                FAIR ENTRANCE{" "}
+              </span>
+              <span className="col-span-2  ">FREE </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CITY TOUR</span>
+              <span className=" ">79 €</span>
+              <span className=" ">87 €</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CONFERENCE</span>
+              <span className=" ">169 €</span>
+              <span className=" ">205 €</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                GALA DINNER{" "}
+              </span>
+              <span className=" ">149 €</span>
+              <span className=" ">175 €</span>
+            </div>
+
+            <div className="fee-row md:h-[84px] md:items-center	">
+              <span className="col-span-1 text-left font-bold ">
+                PACKAGE 1:{" "}
+                <span className="text-pink text-[12px] font-normal block md:inline">
+                  CONFERENCE, WORKSHOPS
+                </span>
+              </span>
+              <span className=" ">279 €</span>
+              <span className=" ">321 €</span>
+            </div>
+            <div className="fee-row md:h-[108px] md:items-center">
+              <span className="col-span-1 text-left font-bold">
+                PACKAGE 2:{" "}
+                <span className="text-pink text-[12px] font-normal block md:inline">
+                  {" "}
+                  CONFERENCE, WORKSHOPS, GALA DINNER
+                </span>
+              </span>
+              <span className=" ">409 €</span>
+              <span className=" ">467 €</span>
+            </div>
+          </div>
+          <div className="fee-col">
+            <div className="fee-header">
+              <span className="col-span-2  font-bold">
+                WE MEMBERS & NATIONAL <br /> ASSOCIATIONS
+              </span>
+              <span className=" text-sm text-center">
+                EARLY BIRD RATES <br />
+                IF BOOKED BEFORE <br />
+                <strong>30 SEPTEMBER</strong>{" "}
+              </span>
+              <span className=" text-sm text-center">
+                REGULAR RATES <br /> FROM <br />
+                <strong>1 OCTOBER</strong>{" "}
+              </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                FAIR ENTRANCE{" "}
+              </span>
+              <span className="col-span-2  ">FREE </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CITY TOUR</span>
+              <span className=" ">79 €</span>
+              <span className=" ">87 €</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CONFERENCE</span>
+              <span className=" ">198 €</span>
+              <span className=" ">228 €</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                GALA DINNER{" "}
+              </span>
+              <span className=" ">149 €</span>
+              <span className=" ">175 €</span>
+            </div>
+            <div className="fee-row md:h-[84px] md:items-center">
+              <span className="col-span-1 text-left font-bold ">
+                PACKAGE 1:{" "}
+                <span className="text-pink text-[12px] font-normal block md:inline">
+                  CONFERENCE, WORKSHOPS
+                </span>
+              </span>
+              <span className=" ">339 €</span>
+              <span className=" ">392 €</span>
+            </div>
+            <div className="fee-row md:h-[108px] md:items-center">
+              <span className="col-span-1 text-left font-bold">
+                PACKAGE 2:{" "}
+                <span className="text-pink text-[12px] font-normal block md:inline">
+                  {" "}
+                  CONFERENCE, WORKSHOPS, GALA DINNER
+                </span>
+              </span>
+              <span className=" ">489 €</span>
+              <span className=" ">567 €</span>
+            </div>
+          </div>
+          <div className="fee-col">
+            <div className="fee-header">
+              <span className="font-bold">
+                NON-WE <br /> MEMBERS
+              </span>
+              <span className=" text-sm text-center">
+                EARLY BIRD RATES <br />
+                IF BOOKED BEFORE <br />
+                <strong>30 SEPTEMBER</strong>{" "}
+              </span>
+              <span className=" text-sm text-center">
+                REGULAR RATES <br /> FROM <br />
+                <strong>1 OCTOBER</strong>{" "}
+              </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                FAIR ENTRANCE
+              </span>
+              <span className="col-span-2  ">FREE </span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CITY TOUR</span>
+              <span className=" ">91 €</span>
+              <span className=" ">105 €</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">CONFERENCE</span>
+              <span className=" ">223 €</span>
+              <span className=" ">257 €</span>
+            </div>
+            <div className="fee-row">
+              <span className="col-span-1 text-left font-bold">
+                GALA DINNER{" "}
+              </span>
+              <span className=" ">198 €</span>
+              <span className=" ">228 €</span>
+            </div>
+
+            <div className="fee-row md:h-[84px] md:items-center">
+              <span className="col-span-1 text-left font-bold ">
+                PACKAGE 1:{" "}
+                <span className="text-pink text-[12px] font-normal block md:inline">
+                  CONFERENCE, WORKSHOPS
+                </span>
+              </span>
+              <span className=" ">369 €</span>
+              <span className=" ">421 €</span>
+            </div>
+            <div className="fee-row md:h-[108px] md:items-center">
+              <span className="col-span-1 text-left font-bold">
+                PACKAGE 2:{" "}
+                <span className="text-pink text-[12px] font-normal block md:inline">
+                  {" "}
+                  CONFERENCE, WORKSHOPS, GALA DINNER
+                </span>
+              </span>
+              <span className=" ">531 €</span>
+              <span className=" ">613 €</span>
+            </div>
+          </div>
         </div>
+        <div></div>
       </div>
       <div className="container">
-        <div className="addition-header mt-12 py-4 px-6 text-white bg-blue font-bold">
+        <div className="addition-header mt-12 py-4 px-6 text-white bg-blue font-bold text-center">
           SUPER PACKAGES
         </div>
         <div className="addition-tab grid grid-cols-8 bg-bggray">
@@ -226,10 +286,6 @@ const Fee = () => {
             YOU WILL RECEIVE 3 TICKETS FREE OF CHARGE
           </span>
         </div>
-        <h3 className="text-pink uppercase text-2xl my-12">
-          ATTENTION: ALL PRICES ARE NET. VAT WILL BE ADDED ACCORDING TO LOCAL
-          REGULATIONS.
-        </h3>
       </div>
     </section>
   );
